d0eceac5f109c1c63cd3c42b05fca9effc117d42
JeminPARK <92086060+JeminPARK@users.noreply.github.com>
2023-06-19 23:12:01 +0900

    Update practice.sql

--- .github/workflows/practice.sql
+++ .github/workflows/practice.sql
@@ -1,6 +1,6 @@
 
 -- hackerank long short city name
 select city, length(city) from station order by length(city) asc, city limit 1;
- select city, length(city) from station order by length(city) desc, city limit 1;
+select city, length(city) from station order by length(city) desc, city limit 1;
 
- WITH SHORT AS( SELECT CITY, LENGTH(CITY) FROM STATION GROUP BY CITY ORDER BY LENGTH(CITY),CITY LIMIT 1), lONGEST AS(SELECT CITY, LENGTH(CITY) FROM STATION GROUP BY CITY ORDER BY lENGTH(CITY) DESC, CITY LIMIT 1) SELECT * FROM SHORT UNION ALL SELECT * FROM lONGEST
+WITH SHORT AS( SELECT CITY, LENGTH(CITY) FROM STATION GROUP BY CITY ORDER BY LENGTH(CITY),CITY LIMIT 1), lONGEST AS(SELECT CITY, LENGTH(CITY) FROM STATION GROUP BY CITY ORDER BY lENGTH(CITY) DESC, CITY LIMIT 1) SELECT * FROM SHORT UNION ALL SELECT * FROM lONGEST
