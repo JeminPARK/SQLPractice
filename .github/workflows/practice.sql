c04be7e9abf0a581d9ec1a1e98ea36b9af76b5b1
JeminPARK <92086060+JeminPARK@users.noreply.github.com>
2023-06-23 00:04:39 +0900

    sql LEFT() function

--- .github/workflows/practice.sql
+++ .github/workflows/practice.sql
@@ -4,3 +4,8 @@ select city, length(city) from station order by length(city) asc, city limit 1;
 select city, length(city) from station order by length(city) desc, city limit 1;
 
 WITH SHORT AS( SELECT CITY, LENGTH(CITY) FROM STATION GROUP BY CITY ORDER BY LENGTH(CITY),CITY LIMIT 1), lONGEST AS(SELECT CITY, LENGTH(CITY) FROM STATION GROUP BY CITY ORDER BY lENGTH(CITY) DESC, CITY LIMIT 1) SELECT * FROM SHORT UNION ALL SELECT * FROM lONGEST
+
+
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+select city from station where LEFT(city,1) IN ('i.e.', 'a', 'e','i', 'o', 'u');    
+
